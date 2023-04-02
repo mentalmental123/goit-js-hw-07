79cd38a8640bce319f3d006bf5710b2e36d67c2d
Alexey Luzanchuk <Luzanchuk123@gmail.com>
2023-04-02 15:48:53 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const markup = galleryItems.map(item => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`).join('');
+
+const refs = {
+    list: document.querySelector('.gallery'),
+    link: document.querySelector('.gallery__link')
+}
+refs.list.insertAdjacentHTML('afterbegin', markup);
+
+refs.list.addEventListener('click', getLargerImage);
+function getLargerImage(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    const largerImgLink = evt.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+    <img src="${largerImgLink}" width="800" height="600">
+    `,{
+	onShow: (instance) => {document.addEventListener('keydown', getEscp)},
+	/*
+	 * Function that gets executed before the lightbox closes.
+	 * Returning false will prevent the lightbox from closing.
+	 */
+	onClose: (instance) => {document.removeEventListener('keydown', getEscp)}
+})
+
+    instance.show()
+    
+    function getEscp(evt) {
+        if (evt.code !== 'Escape') {
+            return;
+        }
+        instance.close();
+        document.removeEventListener('keydown', getEscp)
+    }
+    
+    
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const markup = galleryItems.map(item => `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>
+`).join('');
+
+const refs = {
+    list: document.querySelector('.gallery'),
+}
+refs.list.insertAdjacentHTML('afterbegin', markup);
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+   captionsData: 'alt',
+   captionDelay : 250
+});
+
